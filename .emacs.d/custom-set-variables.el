eb866ac420027bee6b07b05074e46225a4cb69a2
jorden <freewm@163.com>
2019-05-09 22:22:24 +0800

    add elpa

---
+++ .emacs.d/custom-set-variables.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(git-gutter:handled-backends (quote (svn hg git)))
+ '(package-selected-packages
+   (quote
+    (tao-theme challenger-deep-theme distinguished-theme badger-theme hemisu-theme heroku-theme cherry-blossom-theme bubbleberry-theme busybee-theme dakrone-theme soothe-theme dracula-theme darkburn-theme clues-theme flatland-theme ample-zen-theme gruber-darker-theme tangotango-theme grandshell-theme alect-themes gruvbox-theme gotham-theme ample-theme cyberpunk-theme moe-theme molokai-theme monokai-theme color-theme-sanityinc-tomorrow color-theme-sanityinc-solarized zenburn-theme magit nov vimrc-mode visual-regexp stripe-buffer color-theme elpa-mirror toc-org shackle adoc-mode keyfreq auto-package-update counsel-css slime evil-textobj-syntax evil-args evil-lion evil-visualstar evil-surround evil-nerd-commenter evil-matchit evil-mark-replace evil-iedit-state evil-find-char-pinyin evil-exchange evil-escape evil undo-tree jss websocket bash-completion ace-pinyin iedit package-lint emms dumb-jump web-mode pyim ivy-hydra hydra neotree simple-httpd legalese elpy company-statistics company-c-headers company yasnippet-snippets cliphist git-link tagedit rjsx-mode js2-mode js-doc multi-term rvm flymake-lua pomodoro bbdb flyspell-lazy cpputils-cmake cmake-mode ace-window buffer-move counsel-gtags w3m unfill session winum emmet-mode groovy-mode regex-tool command-log-mode ibuffer-vc counsel-bbdb find-file-in-project counsel swiper ivy flymake-jslint exec-path-from-shell git-timemachine dsvn textile-mode rainbow-delimiters scratch diminish htmlize dictionary connection link markdown-mode scss-mode haml-mode writeroom-mode nvm jump find-by-pinyin-dired pinyinlib findr paredit workgroups2 lua-mode request wgrep gist gitconfig-mode gitignore-mode fringe-helper expand-region ace-link auto-yasnippet avy smex async)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
